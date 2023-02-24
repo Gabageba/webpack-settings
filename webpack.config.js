fb491555e811e975afaf74d8784f0baf9404ed8c
Alexandr Panteleev <88164077+Gabageba@users.noreply.github.com>
2023-02-24 23:10:30 +0700

    fonts connection

--- webpack.config.js
+++ webpack.config.js
@@ -55,7 +55,11 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i, //обработка изображений
         type: 'asset/resource',
-      }
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i, //обработка шрифтов
+        type: 'asset/resource',
+      },
     ]
   }
 }
